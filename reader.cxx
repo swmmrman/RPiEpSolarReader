e71423b65599c4411289f06d27744eb8e211eed3
Stephan Kern <stephan@swmmrman.com>
2018-05-31 00:13:07 -0600

    Fixed spelling/Format errors

--- reader.cxx
+++ reader.cxx
@@ -11,7 +11,7 @@ int main(int argc, char *argv[]) {
   modbus_t *ctx;
   int errno;
   if(argc < 2) {
-    cout << "usage" << argv[0] << "<path-to-device>\n";
+    cout << "Usage: " << argv[0] << "<path-to-device>\n";
     return 1;
   }
   ctx = modbus_new_rtu(argv[1], 115200, 'N', 8, 1);
@@ -28,20 +28,18 @@ int main(int argc, char *argv[]) {
   /*Not needed if USB->RS485 See https://github.com/stephane/libmodbus/issues/316
   res = modbus_rtu_set_serial_mode(ctx, MODBUS_RTU_RS485);
   if(res == -1) {
-	  cout << "Failed to set serial mode: " << modbus_strerror(errno) << 
+	  cout << "Failed to set serial mode: " << modbus_strerror(errno) <<
 			"\n";
-	  goto exit;	
+	  goto exit;
   }*/
   if(modbus_set_slave(ctx, 1) == -1) {
-	  cout << "Unable to set slave id: " << modbus_strerror(errno) << 
+	  cout << "Unable to set slave id: " << modbus_strerror(errno) <<
 			"\n";
 	  goto exit;
   }
 
   return 0;
-  exit: 
+  exit:
 	modbus_free(ctx);
 	return 1;
 }
-
-
